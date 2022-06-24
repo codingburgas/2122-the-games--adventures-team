58dc639a429f4572060990127c0d8445c0266c3a
Ivan Stoychev <98114211+IYStoychev21@users.noreply.github.com>
2022-06-24 11:44:41 +0300

    Fix site design

--- Website/src/Components/Card.jsx
+++ Website/src/Components/Card.jsx
@@ -1,15 +1,12 @@
-export default function Person(props) {
+export default function Card(props) {
   return (
     <>
-      <div className="card">
-        <div className="card--name--image">
-          <div className="card__image"></div>
-          <h2 className="card__name">{props.item.name}</h2>
-        </div>
-        <div className="card__info">
-          <p className="card__info__github">Github: {props.item.github}</p>
-          <p className="card__info__outlook">Email: {props.item.outlook}</p>
-        </div>
+      <div className="card--container">
+        <img src="" alt="" className="card__icon" />
+
+        <img src={props.item.img} alt="" className="card__image" />
+
+        <h4 className="card__role">{props.item.role}</h4>
       </div>
     </>
   );
---
+++ Website/src/data/cardData.js
@@ -0,0 +1,44 @@
+let cardData = [
+  {
+    img: "#",
+    role: "Web",
+    id: 0,
+  },
+  {
+    img: "#",
+    role: "Web",
+    id: 1,
+  },
+  {
+    img: "#",
+    role: "Web",
+    id: 2,
+  },
+  {
+    img: "#",
+    role: "Web",
+    id: 3,
+  },
+  {
+    img: "#",
+    role: "Web",
+    id: 4,
+  },
+  {
+    img: "#",
+    role: "Web",
+    id: 5,
+  },
+  {
+    img: "#",
+    role: "Web",
+    id: 6,
+  },
+  {
+    img: "#",
+    role: "Web",
+    id: 7,
+  },
+];
+
+export default cardData;
