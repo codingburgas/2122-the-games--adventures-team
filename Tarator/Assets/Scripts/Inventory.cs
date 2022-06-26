edf8c4d1026e68e0d22429ec3389b3bf98374a23
PPDimitrakova20 <PPDimitrakova20@codingburgas.bg>
2022-06-26 22:00:50 +0300

    Delete TextMeshPro folder

--- Tarator/Assets/Scripts/Inventory.cs
+++ Tarator/Assets/Scripts/Inventory.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Inventory
 {
-       // Initialises an array of game objects 
+       // Initialises an array of game objects
        public List<Items> itemList;
 
-       public Inventory(){
+       public Inventory()
+       {
               itemList = new List<Items>();
        }
 
-       public void addItem (Items item){
+       public void addItem (Items item)
+       {
               itemList.Add(item);
        }
 }
