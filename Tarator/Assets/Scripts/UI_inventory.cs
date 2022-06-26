c109af090e89ed890093073894f73e8cefa8de67
Stefan Ravninov <SORavninov19@codingburgas.bg>
2022-06-26 18:47:32 +0300

    add invetory sys. Close #42

---
+++ Tarator/Assets/Scripts/UI_inventory.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_inventory : MonoBehaviour
+{
+    public Inventory inventory;
+    private Transform MeleeSlot;
+    private Transform RangedSlot;
+    private Transform SpecialSlot;
+    
+    public GameObject MeleeWeapon;
+    public GameObject SpecialWeapon;
+    public GameObject RangedWeapon;
+
+    private void Awake()
+    {
+          MeleeWeapon = GameObject.Find("UI_Staff");
+          MeleeWeapon.SetActive(false);
+
+          SpecialWeapon = GameObject.Find("UI_Vacuum");
+          SpecialWeapon.SetActive(false);
+          
+          RangedWeapon = GameObject.Find("UI_Bow");
+          RangedWeapon.SetActive(false);
+    }
+    public void SetInventory(Inventory inventory)
+    {
+        this.inventory = inventory;
+        RefreshInventoryItems();
+    }
+    
+    private void RefreshInventoryItems(){
+        foreach(Items item in inventory.itemList){
+            if(item.itemType == Items.ItemType.MeleeWeapon){
+               MeleeWeapon.SetActive(true);
+            }
+            if(item.itemType == Items.ItemType.SpecialWeapon){
+                SpecialWeapon.SetActive(true);
+            }
+            if(item.itemType == Items.ItemType.RangedWeapon){
+                RangedWeapon.SetActive(true);
+            }
+        }
+    }
+}
